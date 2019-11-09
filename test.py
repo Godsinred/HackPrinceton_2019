46d08ecced052486de2c9553e25e39df40b035c3
Godsinred <jonathan@nat-oitwireless-inside-vapornet100-10-9-83-15.princeton.edu>
2019-11-09 16:00:53 -0500

    Testing to see if this can move our servo's arm using arduino

---
+++ test.py
@@ -0,0 +1,30 @@
+import pyfirmata
+import time
+
+# don't forget to change the serial port to suit
+# port number of the arduino
+board = pyfirmata.Arduino('/dev/ttyACM0')
+
+# start an iterator thread so
+# serial buffer doesn't overflow
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+# set up pin D9 as Servo Output
+pin9 = board.get_pin('d:9:s')
+
+def move_servo(a):
+    pin9.write(a)
+
+move_servo(90)
+print('end of program')
+
+# board.digital[10].mode = pyfirmata.INPUT
+#
+# while True:
+#     sw = board.digital[10].read()
+#     if sw is True:
+#         board.digital[13].write(1)
+#     else:
+#         board.digital[13].write(0)
+#     time.sleep(0.1)
---
+++ twilio.py
@@ -0,0 +1,40 @@
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+import io
+
+
+def transcribe_file(speech_file):
+    """Transcribe the given audio file."""
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    import io
+    client = speech.SpeechClient()
+
+    # [START speech_python_migration_sync_request]
+    # [START speech_python_migration_config]
+    with io.open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        language_code='en-US',
+        model='default',
+        audio_channel_count=2)
+    # [END speech_python_migration_config]
+
+    # [START speech_python_migration_sync_response]
+    response = client.recognize(config, audio)
+    print(response)
+    # [END speech_python_migration_sync_request]
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+    # [END speech_python_migration_sync_response]
+# [END speech_transcribe_sync]
+
+transcribe_file('/Users/jonathan/Desktop/test.wav')
+print("end of file")
