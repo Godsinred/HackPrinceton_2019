335ae47f374175bf1c96bfa57389e2806e415835
Godsinred <jonathan@nat-oitwireless-inside-vapornet100-10-9-83-15.princeton.edu>
2019-11-09 18:14:04 -0500

    Pushing work assignments

--- twilio.py
+++ twilio.py
@@ -2,7 +2,9 @@ from google.cloud import speech_v1
 from google.cloud.speech_v1 import enums
 import io
 
-
+##############################
+### TEXT TO SPEECH IS HERE ###
+##############################
 def transcribe_file(speech_file):
     """Transcribe the given audio file."""
     from google.cloud import speech
@@ -30,11 +32,46 @@ def transcribe_file(speech_file):
     # [END speech_python_migration_sync_request]
     # Each result is for a consecutive portion of the audio. Iterate through
     # them to get the transcripts for the entire audio file.
+
+    sentence = ''
     for result in response.results:
         # The first alternative is the most likely one for this portion.
         print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+        sentence  += ' ' + result.alternatives[0].transcript
     # [END speech_python_migration_sync_response]
+
+    return sentence.strip()
 # [END speech_transcribe_sync]
 
-transcribe_file('/Users/jonathan/Desktop/test.wav')
-print("end of file")
+sentence = transcribe_file('/Users/jonathan/Desktop/test.wav')
+
+##############################
+### TEXT TO DEGREES HERE #####
+##############################
+# x start position of the current letter being executed
+x_position = 0
+for letter in sentence:
+    # we onlt want to print out characters and skip the rest
+    if letter is ' ':
+        continue
+
+        ### get list of x,y cords for the letter here
+        coords_list = get_coords(letter)
+
+        ### get set of degrees here (all movements for the arm)
+        degrees_list = get_degrees(coords_list)
+
+        ### do simmulation here or in the get degrees function
+        
+
+        ### move arduino arm
+        move_arm(degrees_list)
+
+### get list of x,y cords for the letter here
+def get_coords(letter):
+    pass
+
+
+### get set of degrees here (all movements for the arm)
+def get_degrees(coords_list):
+    pass
